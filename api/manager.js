70ba63ee8f8ecc4d339708f1aec1f960f47ce0ec
Dyoniso_ <pedrog333333@gmail.com>
2021-12-02 15:14:49 -0300

    Fixed Bugs

--- api/manager.js
+++ api/manager.js
@@ -3,31 +3,69 @@ const io = require('../app').io
 const Logger = require('./logger')
 const logger = new Logger('app')
 const db = require('./database')
-const tables = require('./sync').tables
 const pug = require('pug')
 const fm = require('./fileManager')
 const utils = require('./utils')
 
+const tables = require('./sync').tables
+const schema = require('./sync').schema
+
+const POST_MODE_THREAD = 'thread'
+const POST_MODE_REPLY = 'reply'
+
 app.get('/', (req, res) => {
-    return renderThreadView(req, res)
+    return renderLayerView(req, res)
 })
 
 ;(async() => {
     let boards = await db.query(`SELECT * FROM ${tables.BOARD}`)
     for (b of boards) {
         app.get('/' + b.path, (req, res) => {
-            return renderThreadView(req, res, b.path)
+            return renderLayerView(req, res, b.path)
         })
     }
 })()
 
+async function getThreadById(board, id) {
+    if (!id || isNaN(id)) id = -1
+
+    let thread = null
+    if (id > 0) {
+        try {
+            let q = await db.query(`SELECT * FROM ${schema.BOARD}.${board} WHERE id = $1`, id)
+            q = q[0]
+            if (q) thread = formatThread(q)
+
+        } catch (err) {
+            logger.error('Error after get thread by id: '+ id, err)
+        }
+    }
+    return thread
+}
+
+async function getThreadReplies(board, thid, limit) {
+    if (!limit || isNaN(limit)) limit = 25
+    if (!thid || isNaN(thid)) thid = -1
+
+    let replies = []
+    if (thid > 0) {
+        try {
+            let q = await db.query(`SELECT * FROM ${schema.THREAD_REPLY}.${board} WHERE thid = $1 LIMIT $2`, [thid, limit])
+            if (q) for (r of q) replies.push(formatThreadReply(r))
+
+        } catch (err) {
+            logger.error('Error after get thread replies from thid: '+ thid, err)
+        }
+    }
+    return replies
+}
 
 async function getThreads(board, limit, offSet) {
     if (!offSet || isNaN(offSet) || offSet < 0) offSet = 0
 
     let threads = []
     try {
-        let rst = await db.query(`SELECT * FROM ${tables.BOARD}.${board} ORDER BY id DESC OFFSET $2 LIMIT $1`, [limit, offSet])
+        let rst = await db.query(`SELECT * FROM ${schema.BOARD}.${board} ORDER BY id DESC OFFSET $2 LIMIT $1`, [limit, offSet])
         for (t of rst) threads.push(formatThread(t))
 
     } catch (err) {
@@ -59,16 +97,55 @@ function formatThread(thread) {
     return thd
 }
 
+function formatThreadReply(reply) {
+    if (reply.file_info && reply.file_info !== '') reply.file_info = JSON.parse(reply.file_info)
+    let rpy = {
+        id : reply.id,
+        username : reply.username,
+        content : reply.content,
+        rawDate : reply.date,
+        date : utils.formatTimestamp(reply.date),
+    }
+    if (reply.file_info && reply.file_info !== '') {
+        rpy.fileInfo = {
+            name : reply.file_info.name,
+            rawSize : reply.file_info.size,
+            size : utils.formatSize(reply.file_info.size),
+            mime : reply.file_info.mime,
+        }
+    }
+    return rpy
+}
+
 //Socket.io
 io.of('thread').on('connection', async(socket) => {
-    let board = 'b'
+    let handshakeData = socket.request;
+    let board = handshakeData._query['board']
     let threads = await getThreads(board, 10)
 
     let html = ''
     for(t of threads) {
-       html = html + pug.renderFile('./public/pug/templades/threadItem.pug', { thread : t, board : 'b' })
+       html = html + pug.renderFile('./public/pug/templades/itemThread.pug', { thread : t, board : 'b' })
     }
     socket.emit('channel layer thread begin', html)
+
+    socket.on('channel thread connect', async(obj) => {
+        let id = parseInt(obj.thid)
+
+        if (!id || id < 0 || isNaN(id)) id = -1
+        if (id < 0) {} //ERROR MESSAGE
+
+        let thread = await getThreadById(board, id)
+        if (thread) {
+            socket.emit('channel layer reply begin', pug.renderFile('./public/pug/templades/threadView.pug', {
+                thread : thread,
+                board : board,
+            }))
+            socket.insideThread = id
+        } else {
+            //ERROR MESSAGE
+        }
+    })
 
     socket.on('channel layer thread scroll', async(obj) => {
         let total = parseInt(obj.total)
@@ -77,17 +154,20 @@ io.of('thread').on('connection', async(socket) => {
             let threads = await getThreads(board, 10, total)
             let html = ''
             for (t of threads) {
-                html = html + pug.renderFile('./public/pug/templades/threadItem.pug', { thread : t, board : board })
+                html = html + pug.renderFile('./public/pug/templades/itemThread.pug', { thread : t, board : board })
             }
             socket.emit('channel layer thread scroll', html)
         }
     })
 
-    socket.on('channel thread', async(obj) => {
+    async function addPost(obj, mode) {
         let username = obj.username
         let content = obj.content
         let file = obj.file
         let board = obj.path
+        let thid = -1
+
+        if (mode === POST_MODE_REPLY) thid = obj.thid
 
         let fileInfo = ''
         let base64 = ''
@@ -103,36 +183,73 @@ io.of('thread').on('connection', async(socket) => {
         try {
             let q = await db.query(`SELECT id FROM ${tables.BOARD} WHERE path = $1`, [board])
             if (q[0] && q[0].id) {
-                let q = await db.query(`INSERT INTO ${tables.BOARD}.${board}(title,username,content,file_info) VALUES ($1, $2, $3, $4) RETURNING id,date,file_info`, ['', username, content, fileInfo])
+                let q = []
+                
+                if (mode === POST_MODE_REPLY) q = await db.query(`INSERT INTO ${schema.THREAD_REPLY}.${board}(thid, username,content,file_info) VALUES ($1, $2, $3, $4) RETURNING id,date,file_info`, [thid, username, content, fileInfo])
+                else q = await db.query(`INSERT INTO ${schema.BOARD}.${board}(title,username,content,file_info) VALUES ($1, $2, $3, $4) RETURNING id,date,file_info`, ['', username, content, fileInfo])
                 q = q[0]
-                let ctn = content
 
+                let ctn = content
                 if (ctn.length > 120) ctn = ctn.substr(0, 120)
+
                 if (fileInfo && fileInfo !== '') {
                     await fm.registerFile(board, { name : fileInfo.name, base64 : base64 })
                 }
-                logger.ok(`Thread created! No. ${ctn} Title: ${0} Content: ${content}`)
-                
-                let thread = formatThread({
-                    id : q.id,
-                    title : '',
-                    username : username,
-                    content : content,
-                    file_info : q.file_info,
-                    date : q.date,
-                })
 
-                for (socket of io.of('/thread').sockets.values()) {
-                    socket.emit('channel layer thread', pug.renderFile('./public/pug/templades/threadItem.pug', { 
-                        thread : thread,
-                        board : board,
-                    }))
-                }
+                if (POST_MODE_REPLY) {
+                    logger.ok(`Reply: ${q.id} added in thread: ${thid}. Content: ${content}`)
+
+                    let reply = formatThreadReply({
+                        id : q.id,
+                        thid : thid,
+                        username : username,
+                        content : content,
+                        file_info : q.file_info,
+                        date : q.date,
+                    })
+    
+                    for (socket of io.of('/thread').sockets.values()) {
+                        if (socket.insideThread === thid) {
+                            socket.emit('channel layer reply', pug.renderFile('./public/pug/templades/itemReply.pug', { 
+                                reply : reply,
+                                board : board,
+                            }))
+                        }
+                    }
+
+                } else {
+                    logger.ok(`Thread created! No. ${ctn} Title: ${0} Content: ${content}`)
+
+                    let thread = formatThread({
+                        id : q.id,
+                        title : '',
+                        username : username,
+                        content : content,
+                        file_info : q.file_info,
+                        date : q.date,
+                    })
+    
+                    for (socket of io.of('/thread').sockets.values()) {
+                        socket.emit('channel layer thread', pug.renderFile('./public/pug/templades/itemThread.pug', { 
+                            thread : thread,
+                            board : board,
+                        }))
+                    }
+                } 
             }
 
         } catch (err) {
             logger.error('Error in register thread in db', err)
         }
+
+    }
+
+    socket.on('channel add thread reply', async(obj) => {
+        await addPost(obj, POST_MODE_REPLY)
+    })
+
+    socket.on('channel add thread', async(obj) => {
+        await addPost(obj, POST_MODE_THREAD)
     })
 })
 
@@ -148,8 +265,8 @@ async function getBoardsList() {
     return boards
 }
 
-async function renderThreadView(req, res, path) {
-    if (!path) path = 'b'
+async function renderLayerView(req, res, path) {
+    if (!path) path = ''
     const boards = await getBoardsList()
-    return res.render('./threadView.pug', { boards : boards, path : path })
+    return res.render('./layerView.pug', { boards : boards, path : path })
 }
--- public/js/manager.js
+++ public/js/manager.js
@@ -1,49 +1,19 @@
 $(document).ready((e) => {
     let shiftKey = false
-    let socket = io('/thread')
+    let socket = null
     let boardPath = $('#boardPath').val()
+    let defaultName = localStorage.defaultUsername
 
-    $(document).on('keyup keydown', (e) => {
-        shiftKey = e.shiftKey
-    })
+    const PAGE_MODE_THREAD = 'thread'
+    const PAGE_MODE_REPLY = 'reply'
 
-    $('#threadUsername').on('keypress', (e) => {
-        if (e.keyCode === 13) e.preventDefault()
-    })
-    $('#threadContent').on('keypress', (e) => {
-        if (shiftKey === false && e.keyCode === 13) {
-            e.preventDefault()
+    let mode = PAGE_MODE_THREAD
 
-            let file = $('#dragFile').prop('files')[0]
-
-            function sendThreadData(file) {
-                let obj = {
-                    username : $('#threadUsername').val(),
-                    content : $('#threadContent').val(),
-                    file : file,
-                    path : boardPath
-                }
-
-                if (obj.username <= 0) obj.username = 'Anon'
-                emitSocketData('channel thread', obj)
-                clearInput()
-            }
-
-            if (file) {
-                let reader = new FileReader()
-                reader.onload = (b) => {
-                    sendThreadData({ name : file.name, result : b.target.result })
-                }
-                reader.readAsDataURL(file)
-            } else {
-                sendThreadData(null)
-            }
-        }
-    }).on('input', (e) => {
-        let el = $(e.target)
-        el.css('height', '5px')
-        el.css('height', (el.prop('scrollHeight') + 'px'))
-    })
+    if (defaultName === '' || !defaultName) {
+        let defaultNameBegin = 'Anon'
+        localStorage.defaultName = defaultNameBegin
+        defaultName = defaultNameBegin
+    }
 
     function emitSocketData(channel, obj) {
         socket.emit(channel, obj)
@@ -65,10 +35,8 @@ $(document).ready((e) => {
 
     }
 
-    $('#dragFile').on('change', (e) => {
-        let el = e.target
+    function addFile(file) {
         let reader = new FileReader()
-        let file = el.files[0]
 
         reader.onload = (b) => {
             updateFilePreview({
@@ -84,7 +52,7 @@ $(document).ready((e) => {
         reader.id = 1
         reader.filename = file.name
         reader.readAsDataURL(file)
-    })
+    }
 
     function clearInput() {
         $('#threadContent').val('')
@@ -99,20 +67,44 @@ $(document).ready((e) => {
         el.scrollTop(el[0].scrollHeight)
     }
 
-    socket.on('channel layer thread', (obj) => {
-        $('#layerContent').append(obj)
-        scrollLock = false
-    })
-    socket.on('channel layer thread begin', (obj) => {
-        $('#layerContent').html(obj)
-    })
-    socket.on('channel layer thread scroll', (obj) => {
-        if (obj !== '') {
-            $('#layerContent').prepend(obj)
-            .scrollTop($(scrollAnchor).offset().top)
-        }
-        setTimeout(() => scrollFetched = true, 3000)
-    })
+    let ltLayerThread = 'channel layer thread'
+    let ltThreadBegin = 'channel layer thread begin'
+    let ltLayerScroll = 'channel layer thread scroll'
+    let ltReplyBegin = 'channel layer reply begin'
+    let ltReply = 'channel layer reply'
+
+    function updateSocketListeners() {
+        socket.off(ltReplyBegin).on(ltReplyBegin, (obj) => {
+            console.log(obj)
+        })
+        socket.off(ltReplyBegin).on(ltReplyBegin, (obj) => {
+            $('#layerContent').html(obj)
+        })
+        socket.off(ltLayerThread).on(ltLayerThread, (obj) => {
+            $('#layerContent').append(obj)
+            scrollLock = false
+        })
+        socket.off(ltThreadBegin).on(ltThreadBegin, (obj) => {
+            $('#layerContent').html(obj)
+            updateListeners()
+        })
+        socket.off(ltLayerScroll).on(ltLayerScroll, (obj) => {
+            if (obj !== '') {
+                $('#layerContent').prepend(obj)
+                .scrollTop($(scrollAnchor).offset().top)
+            }
+            setTimeout(() => scrollFetched = true, 3000)
+        })
+    }
+
+    function updateListeners() {
+        //Connect Thread
+        $('.user-info').off('click').on('click', (e) => {
+            let id = $(e.target).data('id')
+            socket.emit('channel thread connect', { thid : id })
+            mode = PAGE_MODE_REPLY
+        })
+    }
 
     let scrollLock = false
     let scrollFetched = true
@@ -128,7 +120,126 @@ $(document).ready((e) => {
         if (e.originalEvent.wheelDelta >= 0) scrollLock = true
     })
 
-    setInterval(() => {
-        if (scrollLock === false) scrollDown()
-    }, 300)
+    $('#boardBtn').on('click', (e) => {
+        let board = $(e.target).data('path')
+        if (board !== boardPath) {
+            socket = io('/thread', { query : { board : board } })
+            socket.on('connect', () => {
+                updateSocketListeners()
+                boardPath = board
+                initApp()
+            })
+
+        } if (mode === PAGE_MODE_REPLY) {
+
+        }
+        mode = PAGE_MODE_THREAD
+    })
+
+    function initApp() {
+        $('#settingsUsername').val(defaultName)
+        $('#postInput').slideToggle(100)
+
+        $('#threadUsername').on('keypress', (e) => {
+            if (e.keyCode === 13) e.preventDefault()
+        })
+        $('#threadContent').on('keypress', (e) => {
+            if (shiftKey === false && e.keyCode === 13) {
+                e.preventDefault()
+    
+                let file = $('#dragFile').prop('files')[0]
+    
+                function sendPostData(file) {
+                    let obj = {
+                        username : $('#threadUsername').val(),
+                        content : $('#threadContent').val(),
+                        file : file,
+                        path : boardPath
+                    }
+    
+                    if (obj.username <= 0) obj.username = 'Anon'
+    
+                    let postType = 'channel add thread'
+                    if (mode === PAGE_MODE_REPLY) postType = 'channel add thread reply'
+                    emitSocketData(postType, obj) 
+                    clearInput()
+                }
+    
+                if (file) {
+                    let reader = new FileReader()
+                    reader.onload = (b) => {
+                        sendPostData({ name : file.name, result : b.target.result })
+                    }
+                    reader.readAsDataURL(file)
+                } else {
+                    sendPostData(null)
+                }
+            }
+        }).on('input', (e) => {
+            let el = $(e.target)
+            el.css('height', '5px')
+            el.css('height', (el.prop('scrollHeight') + 'px'))
+        })
+
+        $('.logo').on('click', (e) => {
+            socket.disconnect()
+            selectedBoard = ''
+        })
+
+        $('#settingsBtn').on('click', (e) => {
+            let el = $('.settings-box')
+            el.slideToggle(100)
+            $('#settingsUsername').val(defaultName)
+        })
+
+        $('#dragFile').on('change', (e) => {
+            addFile(e.target.files[0])
+        })
+
+        $(document).on('keyup keydown', (e) => {
+            shiftKey = e.shiftKey
+
+        }).on('dragleave', (e) => {
+            e.preventDefault()
+            $('#layerContent').removeClass('drag-file-preview')
+
+        }).on('drop', (e) => {
+            $('#layerContent').removeClass('drag-file-preview')
+            e.preventDefault()
+    
+            let dt = e.originalEvent.dataTransfer
+            if (dt && dt.files.length) {
+                addFile(dt.files[0])
+            }
+
+        }).on('dragover', (e) => {
+            e.preventDefault()
+            $('#layerContent').addClass('drag-file-preview')
+
+        }).on('click', (e) => {
+            let el = $(e.target)
+            if (el.attr('id') !== 'settingsUsername'
+                && el.attr('id') !== 'settingsBtn' && $('.settings-box').is(':visible')) {
+                    let username = $('#settingsUsername').val()
+                    if (!username || username === '') username = 'Anon'
+                    localStorage.defaultUsername = username
+                    defaultName = username
+                    $('.settings-box').slideToggle(100)
+            }
+        })
+
+        setInterval(() => {
+            if (scrollLock === false) scrollDown()
+        }, 300)
+    }
+
+    function finalizeApp() {
+        $('#postInput').slideToggle(100)
+
+        $('#threadContent').off('keypress')
+        $('#threadUsername').off('keypress')
+        $('.logo').off('click')
+        $(document).off('click').off('dragover').off('dragleave').off('drop')
+        .off('keyup keydown')
+    }
 })
